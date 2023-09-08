d1247b1e120735c421ec1b54728f1305d71a6711
BMDragon <bmweiss10@gmail.com>
2023-09-08 03:47:04 -0400

    Calculate relative distance between coordinates

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,16 @@ public class Main {
         Node b;
     }
 
+    private static final int DEGREES_PER_RADIAN = 180;
+
     public static void main(String[] args) {
 
+    }
+
+    public static double greatCircleDistance(Node A, Node B) {
+        double aLat = A.latitude / DEGREES_PER_RADIAN, bLat = B.latitude / DEGREES_PER_RADIAN;
+        double aLong = A.longitude / DEGREES_PER_RADIAN, bLong = B.longitude / DEGREES_PER_RADIAN;
+        return Math.acos(Math.cos(aLat) * Math.cos(bLat) * Math.cos(aLong - bLong) +
+                Math.sin(aLat) * Math.sin(bLat));
     }
 }
