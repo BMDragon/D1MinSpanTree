32514401754018aa5bfe7e864ba304bbf13e1c0c
BMDragon <bmweiss10@gmail.com>
2023-09-08 03:34:09 -0400

    Create Node and Edge classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    class Node {
+        double longitude;
+        double latitude;
+    }
+
+    class Edge {
+        Node a;
+        Node b;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
