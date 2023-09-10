5a403ee35c70adf7664e420dde533ef781bb3b7d
BMDragon <bmweiss10@gmail.com>
2023-09-09 22:54:48 -0400

    Outline methods needed

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     class Node {
         double longitude;
@@ -7,12 +9,34 @@ public class Main {
     class Edge {
         Node a;
         Node b;
+        double length;
     }
 
     private static final int DEGREES_PER_RADIAN = 180;
+    private static final String INPUT_FILE = "../resources/D1Schools.csv";
+
+    private static ArrayList<Node> nodes;
+    private static ArrayList<Edge> edges;
 
     public static void main(String[] args) {
 
+    }
+
+    public static void readCSVFile(String filename) {
+
+    }
+
+    public static void sortNodes() {
+        // Sort by longitude
+    }
+
+    public static void connectEdges() {
+        // Connect all nodes within certain longitude and latitude range
+        // Connect Hawaii to all West Coast schools
+    }
+
+    public static void sortEdges(){
+        // Sort by great circle distance
     }
 
     public static double greatCircleDistance(Node A, Node B) {
@@ -20,5 +44,13 @@ public class Main {
         double aLong = A.longitude / DEGREES_PER_RADIAN, bLong = B.longitude / DEGREES_PER_RADIAN;
         return Math.acos(Math.cos(aLat) * Math.cos(bLat) * Math.cos(aLong - bLong) +
                 Math.sin(aLat) * Math.sin(bLat));
+    }
+
+    public static void minimumSpanningTree() {
+        // Use Prim's algorithm
+    }
+
+    public static void writeCSV() {
+
     }
 }
